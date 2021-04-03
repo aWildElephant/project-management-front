3f0b09866fc32ba52d914afc4b1f7695a209bb10
Etienne Girard <etienne.g.girard@gmail.com>
2021-04-03 18:04:39 +0200

    Generate component for task creation modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateTaskModalComponent } from './create-task-modal/create-task-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTaskModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/create-task-modal/create-task-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-task-modal',
+  templateUrl: './create-task-modal.component.html',
+  styleUrls: ['./create-task-modal.component.sass']
+})
+export class CreateTaskModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
