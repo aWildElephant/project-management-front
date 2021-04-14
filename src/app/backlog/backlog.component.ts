3e3c2c2d1718ff9c159bca86e48593473ab9dbff
Etienne Girard <etienne.g.girard@gmail.com>
2021-04-14 18:52:33 +0200

    Group tasks by status in backlog

--- src/app/backlog/backlog.component.ts
+++ src/app/backlog/backlog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 
-import { Task } from '../backend-client/task.interface'
+import { Status, Task } from '../backend-client/task.interface'
 import { TaskService } from '../backend-client/task.service'
 
 @Component({
@@ -10,11 +10,37 @@ import { TaskService } from '../backend-client/task.service'
 })
 export class BacklogComponent implements OnInit {
 
-  tasks?: Task[]
+  openTasks: Task[] = []
+  inProgressTasks: Task[] = []
+  doneTasks: Task[] = []
 
   constructor(private taskService: TaskService) { }
 
   async ngOnInit(): Promise<void> {
-    this.tasks = await this.taskService.list()
+    const tasks = await this.taskService.list()
+
+    const openTasks: Task[] = []
+    const inProgressTasks: Task[] = []
+    const doneTasks: Task[] = []
+
+    for (let task of tasks) {
+      switch (task.status) {
+        case Status.OPEN:
+          openTasks.push(task)
+          break
+        case Status.IN_PROGRESS:
+          inProgressTasks.push(task)
+          break
+        case Status.DONE:
+          doneTasks.push(task)
+          break
+        default:
+          console.warn(`Invalid task status ${task.status}`)
+      }
+    }
+
+    this.openTasks = openTasks
+    this.inProgressTasks = inProgressTasks
+    this.doneTasks = doneTasks
   }
 }
